3c4b66fee9371a3051f4cf89c5959815ba4a1b93
Daniel Rrapi <uzo1209@gmail.com>
2024-01-31 09:07:31 +0100

    adding ticket class & subscription

--- src/main/java/tms/transport/entities/Ticket.java
+++ src/main/java/tms/transport/entities/Ticket.java
@@ -1,4 +1,20 @@
 package tms.transport.entities;
 
-public class Ticket {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity(name = "tickets")
+public class Ticket extends TransportDocument {
+
+    @Column(name = "checked_date")
+    private LocalDate checkedDate;
+
+    public Ticket() {
+    }
+
+
+
 }
---
+++ src/main/java/tms/transport/entities/TransportDocument.java
@@ -0,0 +1,34 @@
+package tms.transport.entities;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class TransportDocument {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "date_of_emission")
+    private LocalDate dateOfEmission;
+
+    public TransportDocument() {
+    }
+
+    public TransportDocument(LocalDate dateOfEmission) {
+        this.dateOfEmission = dateOfEmission;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public LocalDate getDateOfEmission() {
+        return dateOfEmission;
+    }
+
+    public void setDateOfEmission(LocalDate dateOfEmission) {
+        this.dateOfEmission = dateOfEmission;
+    }
+}
